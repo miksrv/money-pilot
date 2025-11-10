d8469312ba475f0f4d2ef8bbd4036935143a5256
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2025-11-10 10:49:38 -0800

    Fixed default value for CurrencyInput

--- client/src/components/currency-input/CurrencyInput.tsx
+++ client/src/components/currency-input/CurrencyInput.tsx
@@ -1,36 +1,33 @@
-import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react'
+import React, { forwardRef, useEffect, useState } from 'react'
 import { Input } from 'simple-react-ui-kit'
 
 import { Currency } from './types'
 
-export interface CurrencyInputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'value'> {
+export interface CurrencyInputProps
+    extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'value' | 'size'> {
     value?: number | string
     onValueChange?: (value: number | null) => void
     currency: Currency
     locale?: string
 }
 
-const getCurrencyFormat = (value: number, currency: Currency, locale: string) =>
-    new Intl.NumberFormat(locale, {
+const getCurrencyFormat = (value: number, currency: Currency, locale: string) => {
+    if (isNaN(value)) {
+        return ''
+    }
+
+    return new Intl.NumberFormat(locale, {
         style: 'currency',
         currency,
         maximumFractionDigits: 2
     }).format(value)
+}
 
 export const CurrencyInput = forwardRef<HTMLInputElement, CurrencyInputProps>(
-    ({ value, onValueChange, currency, locale = 'ru-RU', ...props }, ref) => {
+    ({ value, onValueChange, currency, locale = 'ru-RU', ...props }) => {
         const [inputValue, setInputValue] = useState<string>(
             value !== undefined && value != null ? getCurrencyFormat(Number(value), currency, locale) : ''
         )
-
-        const inputRef = useRef<HTMLInputElement>(null)
-
-        useImperativeHandle(ref, () => inputRef.current as HTMLInputElement)
-
-        // Sync inputValue with external value prop
-        // useEffect(() => {
-        //
-        // }, [value])
 
         const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
             const sanitized = e.target.value.replace(/[^0-9.,\s]/g, '')
@@ -69,10 +66,15 @@ export const CurrencyInput = forwardRef<HTMLInputElement, CurrencyInputProps>(
             }
         }
 
+        useEffect(() => {
+            if (value !== undefined && inputValue !== String(value)) {
+                setInputValue(getCurrencyFormat(Number(value), currency, locale))
+            }
+        }, [value])
+
         return (
             <Input
                 {...props}
-                ref={inputRef}
                 value={inputValue}
                 onChange={handleChange}
                 onBlur={handleBlur}
